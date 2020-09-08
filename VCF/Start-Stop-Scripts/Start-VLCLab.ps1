b1d142734532158d782cd17b212d6c156be4d982
D'Angelo Karijopawiro <d.karijopawiro@hotmail.com>
2020-09-08 16:23:37 +0200

    Updated script

--- VCF/Start-Stop-Scripts/Start-VLCLab.ps1
+++ VCF/Start-Stop-Scripts/Start-VLCLab.ps1
@@ -67,13 +67,15 @@ $InfraComponents = "fw-1", "dc-1"
 $VCFNodes = "vcf-esxi-1", "vcf-esxi-2", "vcf-esxi-3", "vcf-esxi-4"
 ##Nested Components
 $nHosts = "10.0.10.100", "10.0.10.101", "10.0.10.102", "10.0.10.103"
-$SDDCComponents = "vcenter-mgmt", "nsx-mgmt-1", "sddc-manager"
+$SDDC_Components = "vcenter-mgmt", "nsx-mgmt-1", "sddc-manager"
 $nHost_username = "root"
 $nHost_password = "VMware123!"
+$nHost_Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $nHost_username,(ConvertTo-SecureString -AsPlainText $nHost_password -Force)
 
 ##Connecting to physical host
 try{
-    Connect-VIServer -Server $pHost -Credential $Credentials -ErrorAction Stop
+    Connect-VIServer -Server $pHost -Credential $Credentials -ErrorAction Stop | Out-Null
+    write-log -Value "Connected to $pHost"
 }catch{
     $ErrorMessage = $_.Exception.Message
     write-log -Value $ErrorMessage -ErrorType
@@ -91,15 +93,11 @@ foreach($i in $InfraComponents){
     do{
         try{
             write-log -Value "Checking if VMtools are up and running on VM: $i"
-            $checkVMTools = (Wait-Tools -VM $i -ErrorAction Stop).PowerState
+            $checkVMTools = (Wait-Tools -VM $i -ErrorAction Stop).PowerState 
+            write-log -Value "VM: $i is online!"
         }catch{
             $ErrorMessage = $_.Exception.Message
             write-log -Value $ErrorMessage -ErrorType
-        }finally{
-            if([string]::IsNullOrWhiteSpace($ErrorMessage)){
-                write-log -Value "VM: $i is online!"
-            }
-            $ErrorMessage = $null
         }   
     }while ($checkVMTools -ne "PoweredOn")
 }
@@ -121,37 +119,33 @@ foreach($v in $VCFNodes){
         try{
             write-log -Value "Checking if VMtools are up and running on VM: $v"
             $checkVMTools = (Wait-Tools -VM $v -ErrorAction Stop).PowerState
+            write-log -Value "VM: $v is online!"
         }catch{
             $ErrorMessage = $_.Exception.Message
             write-log -Value $ErrorMessage -ErrorType
-        }finally{
-            if([string]::IsNullOrWhiteSpace($ErrorMessage)){
-                write-log -Value "VM: $v is online!"
-            }
-            $ErrorMessage = $null
-        }   
+        }
     }while ($checkVMTools -ne "PoweredOn")
 }
 
+##Disconnecting from physical host
+try{
+    Disconnect-VIServer -Server * -Confirm:$false -ErrorAction Stop | Out-Null
+    write-log -Value "Disconnected from $pHost"
+}catch{
+    $ErrorMessage = $_.Exception.Message
+    write-log -Value $ErrorMessage -ErrorType
+}
 
-##Remove nested hosts out of maintenance mode!!!!!!------
+##Remove nested hosts out of maintenance mode
 foreach($n in $nHosts){
     try{
-        Connect-VIServer -Server $pHost -Credential $Credentials -ErrorAction Stop
-    }catch{
-        $ErrorMessage = $_.Exception.Message
-        write-log -Value $ErrorMessage -ErrorType
-    }
-    try{
+        Connect-VIServer -Server $n -Credential $nHost_Credentials -ErrorAction Stop | Out-Null
+        write-log -Value "Connected to $n"
         write-log -Value "Removing nested host: $n out of maintenance mode."
-        Set-VMHost -VMHost $n -State Connected -ErrorAction Stop
+        Set-VMHost -VMHost $n -State Connected -ErrorAction Stop | Out-Null
         write-log -Value "Host $n is out of maintenance mode."
-    }catch{
-        $ErrorMessage = $_.Exception.Message
-        write-log -Value $ErrorMessage -ErrorType
-    }
-    try{
-        Disconnect-VIServer -Server * -Force -Confirm:$false
+        Disconnect-VIServer -Server * -Force -Confirm:$false -ErrorAction Stop | Out-Null
+        write-log -Value "Disconnected from $n"
     }catch{
         $ErrorMessage = $_.Exception.Message
         write-log -Value $ErrorMessage -ErrorType
@@ -160,14 +154,24 @@ foreach($n in $nHosts){
 
 Start-Sleep -Seconds 10
 
+##Starting VCF Components
 foreach($Component in $SDDC_Components){
-    foreach($h in $hosts){
-        Connect-VIServer $h -User "root" -Password "VMware123!"
-        if(Get-VM -Name $Component -ErrorAction SilentlyContinue){
-            Start-VM -VM $Component
-            Write-Host "$Component is started on $h." -ForegroundColor Green
+    foreach($n in $nHosts){
+        try{
+            Connect-VIServer $n -Credential $nHost_Credentials -ErrorAction Stop | Out-Null
+            write-log -Value "Connected to $n"
+            if(Get-VM -Name $Component -ErrorAction SilentlyContinue){
+                Start-VM -VM $Component -ErrorAction Stop | Out-Null
+                write-log -Value "$Component is started on ESXI host:  $n."
+            }else{
+                write-log -Value "$Component is not available on ESXI host: $n."
+            }
+            Disconnect-VIServer -Server * -Force -Confirm:$false -ErrorAction Stop
+            write-log -Value "Disconnected from $n"
+        }catch{
+            $ErrorMessage = $_.Exception.Message
+            write-log -Value $ErrorMessage -ErrorType
         }
-        Disconnect-VIServer -Server * -Force -Confirm:$false
     }
 }
 
