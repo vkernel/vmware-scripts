914f4244cd4006d3e59acbf9c4afdd0a19610304
dangelo <d.karijopawiro@hotmail.com>
2025-05-13 09:44:02 +0200

    remove some files

--- terraform/NSX/NSX-Security-Framework/modules/services/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "authorized_flows" {
-  description = "Parsed authorized flows from YAML file"
-  type        = any
-}
-
-variable "tenant_id" {
-  description = "ID of the tenant (e.g., wld09)"
-  type        = string
-} 
--- terraform/NSX/NSX-Security-Framework/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-# NSX Connection Parameters
-nsx_manager_host = "nsx01.lab.lan"
-nsx_username     = "admin"
-nsx_password     = "VMware1!VMware1!"
-
-# Tenants to deploy
-tenants = ["wld09", "wld10"]
